0ba4bb8b566abfeafd006da526326d1730da8c85
nathan ganesh <489290@student.saxion.nl>
2020-11-11 09:24:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
-import Counter from "./Counter";
+import React from 'react';
+import Counter from './Counter';
+import { createStore } from 'redux';
+import reducer from './reducers';
+import { DECREASE, INCREASE, RESET } from './action';
+import { Provider } from 'react-redux';
+
+const defaultState = {
+	count: 73,
+	name: 'hey'
+};
+
+const store = createStore(reducer, defaultState);
+
 const App = () => {
-  return <Counter />;
+	return (
+		<Provider store={store}>
+			<Counter />;
+		</Provider>
+	);
 };
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React from 'react';
+import { connect } from 'react-redux';
+function Counter(props) {
+	const [ count, setCount ] = React.useState(0);
 
-function Counter() {
-  return <h1>counter component</h1>;
+	return (
+		<div className="container">
+			<h1>counter</h1>
+			<p className="counter">{count}</p>
+			<div className="buttons">
+				<button type="button" className="btn" onClick={() => setCount(count - 1)}>
+					decrease
+				</button>
+				<button type="button" className="btn" onClick={() => setCount(0)}>
+					reset
+				</button>
+				<button type="button" className="btn" onClick={() => setCount(count + 1)}>
+					increase
+				</button>
+			</div>
+		</div>
+	);
+}
+function mapStateToProps(state) {
+	return { count: state.count };
 }
 
-export default Counter;
+export default connect(mapStateToProps)(Counter);
